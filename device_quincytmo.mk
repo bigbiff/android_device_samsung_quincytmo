3f09456988a2665630a80b3a1c57b6b5262400d8
bigbiff bigbiff <bigbiff@teamw.in>
2013-02-24 13:42:30 -0500

    update

--- device_quincytmo.mk
+++ device_quincytmo.mk
@@ -20,7 +20,7 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_quincytmo
+PRODUCT_NAME := cm_quincytmo
 PRODUCT_DEVICE := quincytmo
 
 PRODUCT_COPY_FILES := device/samsung/quincytmo/runatboot.sh:recovery/root/sbin/runatboot.sh
